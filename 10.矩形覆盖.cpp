09d3658cd10b37b181cb47a774c16ae512ec34cf
“Huioqy” <baobao381280273@gmail.com>
2019-02-20 17:18:16 +0800

    6-12

---
+++ 10.矩形覆盖.cpp
@@ -0,0 +1,31 @@
+﻿/*矩形覆盖*/
+
+/*
+我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
+*/
+
+/*
+斐波那契数列
+*/
+class Solution {
+public:
+    int rectCover(int number) {
+        int  fn = 1, fn_1 = 0;
+        if (number < 1) {
+            return 0;
+        } else while (number>0 && number--){
+            fn += fn_1;
+            fn_1 = fn - fn_1;
+        }
+        return fn;
+        /*
+        if (number < 1) {
+            return 0;
+        } else if (number == 1 || number == 2) {
+            return number;
+        } else {
+            return rectCover(number-1) + rectCover(number-2);
+        }
+        */
+    }
+};
---
+++ 7.斐波那契数列.cpp
@@ -0,0 +1,25 @@
+﻿/*斐波那契数列*/
+
+/*
+现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。
+斐波那契数列：1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
+F(1)=1，F(2)=1, F(n)=F(n-1)+F(n-2)（n>=3，n∈N*）
+n<=39
+*/
+
+/*
+迭代（Iteration） VS 递归（Recursion）
+每次递归都是未知的,前面的值都要计算
+动态规划 Good
+*/
+class Solution {
+public:
+    int Fibonacci(int n) {
+        int  fn = 0, fn_1 = 1;
+        while (n>0 && n--){
+            fn += fn_1;
+            fn_1 = fn - fn_1;
+        }
+        return fn;
+    }
+};
